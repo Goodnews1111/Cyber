2eca7921f4a78ae9ce739d70a85e3399082dd409
Goodnews1111 <ogbugoodnews74@gmail.com>
2026-01-13 19:46:40 -1100

    Updated Code

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ function myPage(id) {
     if (page) {
         page.style.display = 'block';
             }
-        }
+    } 
+function sub(form) {
+    form.style.display = "none";
+
+    const msg = document.getElementById("message");
+        msg.innerHTML = "Message sent successfully. I will get back to you soon!";
+}
+    
+        
